8df23f8426d23e0ce989250c34419114513c328e
yeray <yeraydoblasbueno@gmail.com>
2026-05-03 19:30:18 +0200

    Builder modified to Spanish

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -54,15 +54,15 @@ public class Date {
   /**
    * Constructs a Date object representing a specific day, month, and year.
    *
-   * @param day The day of the month (1-31).
-   * @param month The month of the year (1-12).
-   * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
+   * @param dia The day of the month (1-31).
+   * @param mes The month of the year (1-12).
+   * @param año The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
   }
 
   /**
